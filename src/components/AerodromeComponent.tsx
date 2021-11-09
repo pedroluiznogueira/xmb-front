e998c11dfbeb58f884caae6c611bc04bd1532d08
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-11-08 22:00:08 -0300

    Calling the service function that fetches data inside my component

--- src/components/AerodromeComponent.tsx
+++ src/components/AerodromeComponent.tsx
@@ -1,7 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import { getAerodromes } from '../services/AerodromeService';
 
 function AerodromeComponent() {
     const [list, setList] = useState([]);
+
+    useEffect(
+        () => {
+            getAerodromes()
+                .then(
+                    (aerodromes: any) => {
+                        setList(aerodromes)
+                        console.log(aerodromes)
+                })
+        }, [])
+
     return(
       <>
       </>
