c2c8d0c4edf49185ec343f5a88c1259b5176fc8b
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-11-09 03:33:22 -0300

    Implemented login functionality and setting the user token in session storage to represent it's session in the system

--- src/components/Login/LoginComponent.js
+++ src/components/Login/LoginComponent.js
@@ -1,15 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { UserLogin } from '../../models/UserLogin';
 import './LoginComponent.css';
+import { login } from '../../services/UsuarioService';
 
 function LoginComponent() {
+    const [emailInput, setEmailInput] = useState('');
+    const [passwordInput, setPasswordInput] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let user = new UserLogin();
+
+        user.email = emailInput;
+        user.password = passwordInput;
+
+        login(user).then(
+          () => {
+            setEmailInput('');
+            setPasswordInput('');
+          }
+        )
+      };
     
     return(
         <div className="container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Username</label>
-                <input type="text"></input>
+                <input type="text" onChange={event => setEmailInput(event.target.value)} value={emailInput}/>
 
                 <label>Password</label>
-                <input type="text"></input>
+                <input type="text" onChange={event => setPasswordInput(event.target.value)} value={passwordInput} />
 
                 <button type="submit">Send</button>
             </form>
--- src/services/UsuarioService.js
+++ src/services/UsuarioService.js
@@ -1,16 +1,33 @@
-export function registrate(usuario) {
-    console.log(usuario)
+export function registrate(user) {
     return fetch('http://localhost:8080/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(usuario)
+        body: JSON.stringify(user)
     })
     .then(
         (data) => {
           data.json()
-          console.log(data)
+        }
+    )
+}
+
+export function login(user) {
+    return fetch('http://localhost:8080/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    .then(
+        (data) => {
+          data.json()
+            .then(
+              (resp) => {
+                window.sessionStorage.setItem('token', JSON.stringify(resp.token))
+            })
         }
     )
 }
